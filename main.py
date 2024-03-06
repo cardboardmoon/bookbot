d32d26272c0b2462e9d64c56ff6002c2b891a3d5
cardboardmoon <jesse@cardboardmoon.games>
2024-03-06 08:24:42 -0800

    bookbot is complete

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     words = get_contents("books/frankenstein.txt").split()
-    print(f"There are {len(words)} words in Frankenstein.")
-    #print(character_count())
-    sort_list(convert_to_list(character_count()))
+    print(f"There are {len(words)} words in Frankenstein.\n")
+    final_list = sort_list(character_count())
+    for _dict in final_list:
+        print("The '{}' character was found {} times".format(_dict["character"], _dict["count"]))
 
 def get_contents(text):
     with open(text) as f:
@@ -12,27 +13,22 @@ def get_contents(text):
 def character_count():
     lowered = get_contents("books/frankenstein.txt").lower()
     chars = list(lowered)
-    char_count = {}
+    char_dict_list = []
     for char in chars:
         if char.isalpha():
-            if char in char_count:
-                char_count["count"] += 1 
+            for dict in char_dict_list:
+                if dict["character"] == char:
+                    dict["count"] += 1
+                    break
             else:
-                char_count["character"] = char
-                char_count["count"] = 1 
-    return char_count
+                char_dict_list.append({"character": char, "count": 1})
+    return char_dict_list
 
 def sort_on(_dict):
     return _dict["count"]
 
-def convert_to_list(_dict):
-    _list = []
-    for i in _dict:
-        _list.append({"character": i, "count": _dict[i]})
+def sort_list(_list):
+    _list.sort(reverse=True, key=sort_on)
     return _list
-
-def sort_list(list):
-    sorted_list = list.sort(reverse=True, key=sort_on)
-    return sorted_list
 
 main()
